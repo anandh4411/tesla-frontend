6e729d2b427abc62bcab7e93fec8cb874277b96f
anandh4411 <anandh4411@gmail.com>
2023-08-30 13:45:06 +0530

    created necessary modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { CarDetailsComponent } from './modules/car-details/car-details.component';
+import { ListingComponent } from './modules/listing/listing.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'car/details',
+    component: CarDetailsComponent,
+    loadChildren: () =>
+      import('./modules/car-details/car-details.module').then((m) => m.CarDetailsModule),
+  },
+  {
+    path: 'car/listing',
+    component: ListingComponent,
+    loadChildren: () =>
+      import('./modules/listing/listing.module').then((m) => m.ListingModule),
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    loadChildren: () =>
+      import('./modules/contact/contact.module').then((m) => m.ContactModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/car-details/car-details.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarDetailsComponent } from './car-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarDetailsModule { }
