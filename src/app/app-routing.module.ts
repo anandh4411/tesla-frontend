02f1c118b98a3cc7c43cc02a2b8994e8b77f357d
anandh4411 <anandh4411@gmail.com>
2023-08-30 19:46:06 +0530

    added template designs to all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './modules/home/home.component';
 import { CarDetailsComponent } from './modules/car-details/car-details.component';
 import { ListingComponent } from './modules/listing/listing.component';
 import { ContactComponent } from './modules/contact/contact.component';
+import { AboutComponent } from './modules/about/about.component';
 
 const routes: Routes = [
   {
@@ -28,6 +29,12 @@ const routes: Routes = [
     component: ListingComponent,
     loadChildren: () =>
       import('./modules/listing/listing.module').then((m) => m.ListingModule),
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    loadChildren: () =>
+      import('./modules/about/about.module').then((m) => m.AboutModule),
   },
   {
     path: 'contact',
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
 
-
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     CommonModule
